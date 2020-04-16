518732e731265062969c151568bf9ce7261cb978
sortheish <you@example.com>
2020-04-17 00:23:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.dinnerdecider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+        val foodList = arrayListOf("South-Indian","Chines","Italian","Biryani")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFood.text = foodList[randomFood]
+        }
+
+        decideBtn.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+             foodList.add(newFood)
+            addFoodText.text.clear()
+        }
+    }
+}
