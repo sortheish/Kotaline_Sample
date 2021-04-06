158cdbfa9e50695d0ff2d6f69818ee2bd16cbed2
unknown <ishwari.s@purplle.com>
2021-04-06 11:49:35 +0530

    added dinner.kt

---
+++ app/src/main/java/com/example/dinnerdecider/Dinner.kt
@@ -0,0 +1,5 @@
+package com.example.dinnerdecider
+
+object Dinner {
+    val name:String = TODO()
+}
--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         decideBtn.setOnClickListener {
             val random = Random()
             val randomFood = random.nextInt(foodList.count())
