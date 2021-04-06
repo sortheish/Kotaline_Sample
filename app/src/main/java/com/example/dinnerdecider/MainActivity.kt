2a4e796f86d8c281a40a0c409a0d52cc69c6542f
unknown <ishwari.s@purplle.com>
2021-04-06 11:07:23 +0530

    added second Chines

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -10,7 +10,7 @@ import java.util.*
 */
 class MainActivity : AppCompatActivity() {
         val foodList = arrayListOf("South-Indian","Chines","Italian","Biryani","North-Indian","Thai","Indonesian","Turkish","Spanish","Moroccan","Japanese","Italian",
-            "South-Indian" )
+            "South-Indian","Chines" )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
